358cd82fa812a22cc925a7f2eb824b4f0a0fa209
Swatisharma175 <56503548+Swatisharma175@users.noreply.github.com>
2023-02-26 00:21:20 +0530

    bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import '../styles/App.css';
 import  BMSNavbar  from './BMSNavbar';
 import Home from './Home';
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
-import Test from './BookTicket';
+
 import Register from './Register';
 import Login from './Login';
 import Favorites from './Favorites';
 import BookTicket from './BookTicket';
 import Payment from './Payment';
+import BookSeat from './BookSeat';
 const App = () => {
 
 
@@ -23,6 +24,7 @@ const App = () => {
           <Route path ="/favorites"  element={<Favorites/>}/>
           <Route path ="/bookticket"  element={<BookTicket/>}/>
           <Route path ="/payment"  element={<Payment/>}/>
+          <Route path ="/bookedseat"  element={<BookSeat/>}/>
         </Routes>
       </Router>
     </div>
